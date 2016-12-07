b6c591d00531a7aa7d88e1fa198e052032a3e12b
sunny <820804517@qq.com>
2016-12-07 16:04:59 +0800

    init

---
+++ index.android.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
+import Home from './js/page/Home'
+AppRegistry.registerComponent("reactTest",()=>Home);
