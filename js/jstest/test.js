186c5c79f18458e984964f305ee3d8f0c389d04c
sunny <820804517@qq.com>
2017-02-15 17:00:59 +0800

    e

---
+++ js/jstest/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wufeifei on 2017/2/14.
+ */
+function Good() {
+    // this.name = "apple";
+    // this.count = 1;
+    // this.showInfo = function () {
+    //     alert("商品：" + this.name);
+    // }
+}
